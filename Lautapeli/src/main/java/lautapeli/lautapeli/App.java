565d5d0b1a6d28d5b05a9cac9fb928e9274503ee
Maerus <maerus.sbkv@gmail.com>
2014-01-15 15:12:56 +0200

    projektin luonti

---
+++ Lautapeli/src/main/java/lautapeli/lautapeli/App.java
@@ -0,0 +1,13 @@
+package lautapeli.lautapeli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
